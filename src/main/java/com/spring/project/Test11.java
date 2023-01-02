bc5b9b8e3e1fb3fcea43a4cc1352249c8a4e6d43
DESKTOP-6G9RLVM\Conrad1301 <jgson02@naver.com>
2023-01-02 15:14:12 +0900

    팀 미션 첫번째 작업

---
+++ src/main/java/com/spring/project/Test11.java
@@ -0,0 +1,10 @@
+package com.spring.project;
+
+public class Test11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("어디까지나 테스트입니다.");
+	}
+
+}
